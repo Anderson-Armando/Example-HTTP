5e506bda57b8a0b677e1cc0b7a86573b35a09bcc
Anderson-Armando <arevalosanderson9@gmail.com>
2024-06-11 12:36:34 -0300

    Exemplo com HttpClient

---
+++ ExemploHTTP/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploHTTP.Models
+{
+   public class Post
+    {
+        //Botão direito, Quick Actions..., Encapsulamento
+        //Forma de criar os atributos/propriedades
+        private int id;
+        public int Id { get => id; set => id = value; }
+
+        //Escrever "prop" e alterar o nome da propriedade gerada
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        
+    }
+}
---
+++ ExemploHTTP/Services/RestServices.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using ExemploHTTP.Models;
+using System.Text.Json;
+
+namespace ExemploHTTP.Services
+{
+    internal class RestServices
+    {
+        private HttpClient client;
+        private Post post;
+        private List<Post> posts;
+        private JsonSerializerOptions _serializerOptions;
+
+        RestServices() 
+        {
+            client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+            
+        }
+
+        public async Task <List<Post>> GetPostsAsync()
+        {
+            Uri uri = new Uri("https://jsonplaceholder.typicode.com/posts");
+
+            HttpResponseMessage response = await _client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                posts = JsonSerializer.Deserialize<List<Post>>(content, _serializerOptions);
+
+            }
+        }
+    }
+}
